f51764f51ca4ff82a80b1eee58062f88915df7ed
Miguel Ángel Vinuesa Alonso <33325707+MiguelAngelVinuesaAlonso@users.noreply.github.com>
2020-12-03 14:00:43 +0100

    Update layouts.js

--- src/layouts.js
+++ src/layouts.js
@@ -70,7 +70,7 @@ module.exports = {
 			"1 2 3",
 			"4 5 6",
 			"7 8 9",
-			"  {alpha} . {zero} {backspace} {next} {accept}"
+			". {zero} {backspace}"
 		]
 	},
 
@@ -87,7 +87,7 @@ module.exports = {
 			"1 2 3",
 			"4 5 6",
 			"7 8 9",
-			"_ - . {zero} {backspace} {next} {accept}"
+			". {zero} {backspace}"
 		]
 	}
 
